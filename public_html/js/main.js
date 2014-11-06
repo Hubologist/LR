802ef97827ef6f7d919b6994123e6606fec3a171
Hubologist <hubologist@gmx.com>
2014-11-06 16:56:17 +0000

    Light Radius 4.0 alpha

---
+++ public_html/js/main.js
@@ -0,0 +1,85 @@
+$(document).ready(function() {
+    //Force reload at top
+    $(this).scrollTop(0);
+    
+    //Basic Home animations
+    $('.box').addClass('animated fadeInUp');
+    $('h1').addClass('animated fadeInLeft');
+    $('.splash').addClass('animated fadeInRight');
+    
+    //Typed sweetness
+    $(function(){
+      	$("#typed").typed({
+		strings: [
+                    "Consistent web design, from concept, to paper, to reality.",
+                    "Content creation, marketing consultancy, web development.",
+                    "Beautiful business plans and presentations."],
+                startDelay: 1100,
+		typeSpeed: 50,
+                loop: true
+      	});
+    });
+    
+   //Bouncy arrow animation loop
+    var $arrow = $('#arrow');
+    
+    setInterval(function() {
+            $arrow.toggleClass('animated bounce');
+    }, 4400);
+                
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 0) { //use `this`, not `document`
+            $('#arrow').css({
+                'display': 'none'
+            });
+        }
+    });
+
+/*
+ * Smooth transitions inside page
+ */
+    
+    $("a.smooth").click(function() {
+        $("html, body").animate({
+            scrollTop: $($(this).attr("href")).offset().top + "px"
+        }, {
+            duration: 500,
+            easing: "swing"
+        });
+        return false;
+    });
+
+    
+/*
+ * Picture always fits div
+ */
+    
+    $('#profile_pic > img').load(function(){
+        // maxWidth, maxHeight, maintain aspect ratio
+        $(this).tailorfit({
+                maxWidth  : this.width,
+                maxHeight : this.height,
+                ratio     : this.width / this.height
+        });
+    });
+    
+/*
+ * Waypoints
+ */
+    
+    $('#profile').waypoint(function() {
+        $('.badge').addClass('animated fadeInUp');
+        $('#picture>img').addClass('animated fadeInRight');
+        
+    }, {offset: '20%'});
+    
+    $('#projects').waypoint(function() {
+        $('.display').addClass('animated fadeInUp');
+    }, {offset: '60%'});
+    
+    $('#contact').waypoint(function() {
+        $('.contact').addClass('animated fadeInUp');
+    }, {offset: '60%'});
+
+});
+
---
+++ readme.txt
@@ -0,0 +1,12 @@
+TODO
+
+Make white strip for profile section
+Select a sensible width for the text section and make it slide over the picture
+Create dialogue mini-game inside the text balloon
+
+1. Intro
+    Three questions
+    Three answers
+
+Make nav menu an actual nav menu with an ul and subsequent li
+Make contact form responsive
