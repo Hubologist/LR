edbc7d28757660e901b2aa7ec5f85bfc16e45e9d
Hubologist <hubologist@gmx.com>
2014-11-12 13:31:36 +0000

    Only 1 parallax working. I need to master this skrollr piece of shit

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -16,6 +16,7 @@ function show(id, value) {
 onReady(function () {
     show('page', true);
     show('load', false);
+    skrollr.get().refresh();
 });
 
 $(document).ready(function() {
@@ -40,6 +41,11 @@ $(document).ready(function() {
                 loop: true
       	});
     });
+    
+    $(window).resize(function() {
+  skroll.destroy();
+  skroll = skrollr.init({forceHeight: false});
+});
     
    //Bouncy arrow animation loop
     var $arrow = $('#arrow');
